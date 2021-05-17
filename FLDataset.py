1e8d9ee28626db62e034fee86b4f4ec56405b99f
ytlee93 <teresa8332yt@gmail.com>
2021-05-17 17:14:16 +0800

    noniid complete

--- FLDataset.py
+++ FLDataset.py
@@ -11,6 +11,10 @@ import random
 import collections
 # from torchvision.transforms import ToTensor
 
+def split(a, n):
+    k, m = divmod(len(a), n)
+    return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
+
 def covidIID(dataset, num_users):
     images = int(len(dataset)/num_users)
     users_dict, indeces = {}, [i for i in range(len(dataset))]
@@ -18,7 +22,7 @@ def covidIID(dataset, num_users):
         np.random.seed(i)
         users_dict[i] = set(np.random.choice(indeces, images, replace=False))
         indeces = list(set(indeces) - users_dict[i])
-
+    
     return users_dict
 
 def covidNonIID(dataset, num_users, c_num, noniid_c):
@@ -33,44 +37,55 @@ def covidNonIID(dataset, num_users, c_num, noniid_c):
     shuffled_indices = np.random.permutation(len(indeces_unsortedlabels[0]))
     indeces_unsortedlabels[0] = indeces_unsortedlabels[0][shuffled_indices]
     indeces_unsortedlabels[1] = indeces_unsortedlabels[1][shuffled_indices]
-    print("indeces_unsortedlabels ", indeces_unsortedlabels)
+#     print("indeces_unsortedlabels ", indeces_unsortedlabels)
     indeces_labels = indeces_unsortedlabels[:, indeces_unsortedlabels[1, :].argsort()]
     indeces = indeces_labels[0, :]
     indeces_labels.astype(int)
     indeces.astype(int)
 #     print(indeces_labels)
     
-#     label list with index
+    # label list with index
     index_label = [[] for i in range(c_num)]
     for i in range(len(indeces_labels[1])):
         index_label[indeces_labels[1][i]].append(indeces_labels[0][i])
-    print("index_label: ", index_label)
+#     print("index_label: ", index_label)
         
     client_classes = []
     comb = []
     for i in list(combinations(list(range(0,c_num)), noniid_c)):
         print(i)
         comb.append(i)
-    print(comb)
+    print("comb ", comb)
     
     # classes of client
     for i in range(num_users):
         client_classes.append(comb[i%c_num])
     client_classes = np.array(client_classes)
     c = client_classes.flatten()
-    print(client_classes)
+    print("client_classes ", client_classes)
     
     # count of labels
     label_count = collections.Counter(c)
-    print(label_count)
+    print("label count ", label_count)
     
-#     for i in range(num_users):
-#         np.random.seed(i)
-#         temp = set(np.random.choice(classes_indx, 2, replace=False))
-#         classes_indx = list(set(classes_indx) - temp)
-#         for t in temp:
-#             users_dict[i] = np.concatenate((users_dict[i], indeces[t*images:(t+1)*images]), axis=0)
-#     return users_dict
+    for i in range(len(label_count)):
+        index_label[i] = split(index_label[i], label_count[i])
+        index_label[i] = list(index_label[i])
+        
+    temp = []
+    users_dict = dict.fromkeys(range(10), [])
+    print("users_dict ", users_dict)
+    for i in range(len(client_classes)):
+        for j in range(len(client_classes[i])):
+            cur_cls = client_classes[i][j]
+            temp = index_label[cur_cls].pop()
+    #         print("temp ", temp)
+            users_dict[i] = np.concatenate((users_dict[i], np.array(temp)), axis=0).astype(int)
+    
+    for i in range(len(users_dict)):
+        users_dict[i] = set(users_dict[i])
+    
+    return users_dict
 
 def covidNonIIDUnequal(dataset, num_users):
     classes, images = 1200, 50
